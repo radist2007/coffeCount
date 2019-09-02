e3fd1aba76419b32eebc4e473b5b6a051a432961
Alexander <izofatov2007@gmail.com>
2019-09-03 02:54:58 +0300

    add gh-pages

--- coffeCountServer.js
+++ coffeCountServer.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const path = require('path');
+var ghpages = require('gh-pages');
 
 const app = express();
+
+ 
+ghpages.publish('frontend/Public', function(err) {
+
+	if(err){ console.log(err); }
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname+'/frontend/Public/index.html'));
