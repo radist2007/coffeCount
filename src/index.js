8d5dce8a62ae49557d9cf7ad125757e1d6233a57
Radist2007 <izofatov2007@gmail.com>
2019-09-15 03:19:19 +0300

    add materialize

--- src/index.js
+++ src/index.js
@@ -1,5 +1,53 @@
 import Main from './js/main.js';
 
+document.addEventListener('DOMContentLoaded', function() {
+	console.log("Materialcss");
+	var el    = document.querySelector('#tabs');
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, { });
+    var instance = M.Tabs.init(el, {});
+	var instance = M.Carousel.init({
+		fullWidth: true,
+		indicators: true
+	});
+
+	var elems = document.querySelectorAll('.fixed-action-btn');
+	var instances = M.FloatingActionButton.init(elems, {});
+
+	var elems = document.querySelectorAll('.collapsible');
+	var instances = M.Collapsible.init(elems, {});
+});
+
+  document.addEventListener('DOMContentLoaded', function() {
+			    });
+
+  // Or with jQuery
+  //
+  //   $(document).ready(function(){
+  //       $('.fixed-action-btn').floatingActionButton();
+  //         });
+
+  // Or with jQuery
+  //
+  //   $('.carousel.carousel-slider').carousel({
+  //       fullWidth: true,
+  //           indicators: true
+  //             });
+
+  // Or with jQuery
+  //
+  //   $(document).ready(function(){
+  //       $('.sidenav').sidenav();
+  //         });
+  //
+
+
+    // Or with jQuery
+	//
+	//   $(document).ready(function(){
+	//       $('.tabs').tabs();
+	//         });
+	//               
 console.log("index.js");
 
 var main = new Main("hm...");
